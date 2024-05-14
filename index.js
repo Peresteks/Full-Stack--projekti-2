334f0e6ab8f5f902f0313fb4b844c628473520bc
Mikko Salminen <115707134+Peresteks@users.noreply.github.com>
2024-05-14 22:48:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
+require("dotenv").config();
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
 const app = express();
-app.use(bodyParser.json());
+const mongoose = require('mongoose');
+var url = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000;
 
-// Routes
-const firstroutes = require('./routes/firstroutes');
-app.use(firstroutes);
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/your-database-name', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', error => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
+// Yhdistetään MongoDB-tietokantaan
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Yhteys tietokantaan muodostettu');
+    
+    })
+    .catch((err) => {
+        console.error('Virhe yhdistettäessä tietokantaan: $(err.stack)');
+    });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, function () {
+    console.log("Kuunnellaan porttia...: " + PORT);
+});
