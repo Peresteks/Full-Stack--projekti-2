961c2c9b3fd0c364195b824bb6c2af9e3da704db
Mikko Salminen <115707134+Peresteks@users.noreply.github.com>
2024-05-14 22:44:22 +0300

    Delete models directory

--- models/firstmodel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const firstschema = new mongoose.Schema({
-  // Define your schema fields here
-});
-
-const firstmodel = mongoose.model('firstmodel', firstschema);
-
-module.exports = firstmodel;
